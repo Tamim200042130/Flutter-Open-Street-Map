f2c9b41b82926027d6e48f7514ee303517c5c70f
Tamim200042130 <95132931+Tamim200042130@users.noreply.github.com>
2024-06-11 07:43:15 +0600

    markers-polyline-osm

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_open_street_map/samples/maps/shape_layer/selection/selection.dart';
-import 'package:flutter_open_street_map/samples/maps/shape_layer/sublayer/sublayer.dart';
-import 'package:flutter_open_street_map/samples/maps/tile_layer/bing_map/bing_map.dart';
 import 'package:flutter_open_street_map/samples/maps/tile_layer/open_street_map/open_street_map.dart';
 import 'package:flutter_open_street_map/samples/maps/tile_layer/vector_layer/polylines.dart';
 
@@ -17,16 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: MapPolylinesPage(Key('poly-lines')),
-
+      home: const MapPolylinesPage(
+        Key('polylines'),
+      ),
       // home: const MapOSMPage(
       //   Key('osm'),
-      // ),
-      // home: const MapSublayerPage(
-      //   Key('sublayer'),
-      // ),
-      // home: const MapSelectionPage(
-      //   Key('selection'),
       // ),
     );
   }
